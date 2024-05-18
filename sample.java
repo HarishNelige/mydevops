f91394c693edb9f43ac68468860e5589b936c159
HarishNelige <nelige.harish22@gmail.com>
2024-05-18 22:37:12 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,3 +1,10 @@
-Jave file for checkiin
-
-#Java Code Starts here
+//my Jenkins Sample Java File
+class Helloworld
+  {
+    public static void main(String []args)
+    { 
+    system.out.printIn("Java Sample Code that runs from Jenkins")
+      }
+  };
+//execute java file today with build step.
+//this is the demo before adding build step.
