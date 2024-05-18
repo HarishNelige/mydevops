1a08938d7032506f79384d946d735e22febe12f5
HarishNelige <nelige.harish22@gmail.com>
2024-05-18 22:57:52 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -4,6 +4,7 @@ class Helloworld
     public static void main(String []args)
     { 
     System.out.printIn("Java Sample Code that runs from Jenkins.");
+      System.out.printIn("Adding second line to check poll SCM option");
       }
   };
 //Execute java file today with build step.
